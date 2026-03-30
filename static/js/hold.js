bc021eb8cc0951ff78e98f600df37a2228a7de65
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-03-30 20:25:33 +0800

    Update hold.js

--- static/js/hold.js
+++ static/js/hold.js
@@ -123,7 +123,7 @@ function show_order_list() {
             html += '<li ng-repeat="o" class="" style="display: flex;">\
                 <section style="flex:1;width:auto;">\
                     <p style="margin: 0;display: flex;">\
-                        <span class="ng-binding" style="width:auto;margin-right: 3px;">' + v.ptitle + '</span>\
+                        <span class="ng-binding" style="width:auto;margin-right: 5px;">' + v.ptitle + '</span>\
                         <span style="align-items: center;display: flex;" class="ng-binding ' + ostyle_class2 + '"><i class="' + ostyle_class + '"></i>' + ostyle_name + '（￥' + v.fee + '）</span>\
                     </p>\
                     <p style="margin: 0" class="ng-binding">\
